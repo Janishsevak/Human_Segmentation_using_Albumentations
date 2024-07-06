4034c95e04e662fcdc45242bfbacdeb8137b7819
Janishsevak <jenishsevak7@gmail.com>
2024-07-06 23:27:00 +0530

    setup and requirements file updated

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="Human-Segmentation-Classification-With-PyTorch",
+    version="0.1",
+    author="Jainishsevak",
+    author_email="jenishsevak7@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+'''
+setup.py is a script used in Python packaging to specify the distribution meta-data and dependencies for a project
+
+It is typically used to build and distribute packages for installation via tools like pip.
+
+'''
