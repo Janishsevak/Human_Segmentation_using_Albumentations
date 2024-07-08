04a76bdcec46a4b97f6a96db828d47e7d6f504ba
Janishsevak <jenishsevak7@gmail.com>
2024-07-08 19:17:05 +0530

    Data transformation done

--- src/human/constants/__init__.py
+++ src/human/constants/__init__.py
@@ -11,3 +11,21 @@ DATA_INGESTION_ARTIFACTS_DIR: str = "data_ingestion"
 S3_DATA_FOLDER_NAME: str = "data.zip"
 UNZIPPED_FOLDER_NAME: str = "unzip"
 DATA_DIR_NAME: str = "data"
+
+# constants realted to data transformation
+
+DATA_TRANSFORMATION_ARTIFACTS_DIR : str ="data_transformation"
+CSV_FILE_NAME:str = "train.csv"
+VALIDATION_CSV_FILE = "validation.csv"
+SHUFFLE = True
+PIN_MEMORY = True
+NUM_WORKERS = 0
+IMAGE_SIZE = 320
+BATCH_SIZE = 16
+TEST_SIZE = 0.1
+RANDOM_STATE = 42
+DATALOADER = 'dataloader'
+TRAIN_DATALOADER = 'train_data_loader.pt'
+VALID_DATALOADER = 'valid_data_loader.pt'
+TRANSFORM_OBJECT_NAME: str = "transform.pkl"
+VALID_DIR = 'valid'
--- src/human/entity/artifact_entity.py
+++ src/human/entity/artifact_entity.py
@@ -5,4 +5,12 @@ from dataclasses import dataclass
 class DataIngestionArtifacts:
     data_folder_path :str
 
+# Data transformation artifacts
+@dataclass
+class DataTransformationArtifacts:
+    trainloader_path :str
+    validloader_path :str
+    transformer_object_path :str
+    validation_csv_file :str
+
 
